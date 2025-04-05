0a421648b890246c40e5aabeef00417cf7e55c7b
Daniel <danieleliasdesantanas@gmail.com>
2025-04-04 21:30:13 -0300

    Semãntica

---
+++ aula-bootstrap/main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+// This is a simple Java program that prints "Hello, World!" to the console.
+// It serves as a basic example of how to set up a Java project and run a simple program.   
+console.log("Hello, World!");
+// This is a simple JavaScript program that prints "Hello, World!" to the console.
+
+
